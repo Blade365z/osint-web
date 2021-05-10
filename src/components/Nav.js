55b965c09efbf34bc930f76987a54909d4383a94
blade365z <amitabhyo@gmail.com>
2021-05-11 00:39:16 +0530

    tiles improvised

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,7 +17,7 @@ const Nav = ({ ypos }) => {
                         <img src={logo} />
                     </motion.div>
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon text-white">  <i class="fas fa-bars"></i>   </span>
+                        <span className="navbar-toggler-icon text-white">  <i className="fas fa-bars"></i>   </span>
                     </button>
                     <div className="collapse navbar-collapse " id="navbarContent">
                         <ul className="navbar-nav me-auto m-auto mb-2 mb-lg-0">
